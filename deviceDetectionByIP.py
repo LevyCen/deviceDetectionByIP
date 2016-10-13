d771bc6180be377d715a1edc45fff1b798f0c189
LevyCen <levy_02208@hotmail.com>
2016-10-13 11:18:25 -0500

    Realiza un ping a la direccion de google

--- deviceDetectionByIP.py
+++ deviceDetectionByIP.py
@@ -1,1 +1,10 @@
-print hola
+import os
+
+hostname = "google.com" #example
+response = os.system("ping -c 1 " + hostname)
+
+#and then check the response...
+if response == 0:
+  print hostname, 'is up!'
+else:
+  print hostname, 'is down!'
