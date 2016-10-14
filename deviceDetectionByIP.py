84166c1c98db2338382537c25c69a90f1f3cd06e
LevyCen <levy_02208@hotmail.com>
2016-10-14 13:35:25 -0500

    Ajuste para imprimir los resultados

--- deviceDetectionByIP.py
+++ deviceDetectionByIP.py
@@ -4,7 +4,8 @@ from string import *
 
 diccAP = {} #diccionario vacio
 #Variables para conocer la fecha del registro
-
+apACtivos = int(0)
+apInactivos = int(0)
 ffileCSV = open("apInfo.csv","r")
 
 #Se extrae linea por linea del archivo csv
@@ -19,13 +20,19 @@ for lineaDeCSV in ffileCSV:
     ipPorRevisar= columnaIPAP
     
     #hostname = "google.com" #example
-    
+    print "\n\n"
     print '#############' + columnaNameAP +' - '+ columnaIPAP + '#############'
     hostname = str(columnaIPAP) 
     response = os.system("ping -c 1 " + hostname)
 
     #and then check the response...
     if response == 0:
-    	print '\n'+ columnaNameAP + 'is up!'
+    	print '\n'+ columnaNameAP + ' is up!'
+    	apACtivos=apACtivos + 1
     else:
-    	print '\n'+ columnaNameAP + 'is down!'
+    	print '\n'+ columnaNameAP + ' is down!'
+    	apInactivos = apInactivos + 1
+
+print '------------- Resultados -------------'
+print '------------- AP Activos = '+str(apACtivos) + '-------------'
+print '------------- AP Inactivos = '+str(apInactivos) + '-------------'
