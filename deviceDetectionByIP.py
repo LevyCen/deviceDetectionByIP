9b14a3d92bc7872d412b5a9f5dd6a7232f39bbf9
LevyCen <levy_02208@hotmail.com>
2016-10-14 10:39:09 -0500

    Completamente funcional
    - Lee un archivo CSV que se encuentre en la misma ruta, en este caso el archivo se llama "apInfo.csv".
    - Extrae , uno por uno, cada linea del archivo CSV, ubica la dirrección IP.
    - Ya que tiene la ip, le hace un ping y espera un tiempo.
    - Imprime si el ip contesta o no.

--- deviceDetectionByIP.py
+++ deviceDetectionByIP.py
@@ -1,10 +1,31 @@
 import os
+from os import listdir
+from string import *
 
-hostname = "google.com" #example
-response = os.system("ping -c 1 " + hostname)
+diccAP = {} #diccionario vacio
+#Variables para conocer la fecha del registro
 
-#and then check the response...
-if response == 0:
-  print hostname, 'is up!'
-else:
-  print hostname, 'is down!'
+ffileCSV = open("apInfo.csv","r")
+
+#Se extrae linea por linea del archivo csv
+for lineaDeCSV in ffileCSV:
+	#se acomoda en un arreglo, dividido por las comas
+    lineaLeidaCSV = split(lineaDeCSV,',')
+
+    columnaNameAP =  strip(lineaLeidaCSV[0])
+    columnaMACAP =  strip(lineaLeidaCSV[1])
+    columnaIPAP =  strip(lineaLeidaCSV[2])
+
+    ipPorRevisar= columnaIPAP
+    print 
+
+    #hostname = "google.com" #example
+    print columnaNameAP +'-'+columnaIPAP
+    hostname = str(columnaIPAP) 
+    response = os.system("ping -c 1 " + hostname)
+
+    #and then check the response...
+    if response == 0:
+    	print columnaNameAP + 'is up!'
+    else:
+    	print columnaNameAP + 'is down!'
